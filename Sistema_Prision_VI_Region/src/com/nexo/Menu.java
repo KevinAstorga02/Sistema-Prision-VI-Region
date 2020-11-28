492308904de09acf614cc2072d5a0e5fcb0f04b3
KevinAstorga02 <75091523+KevinAstorga02@users.noreply.github.com>
2020-11-28 20:11:54 -0300

    Add files via upload

---
+++ Sistema_Prision_VI_Region/src/com/nexo/Menu.java
@@ -0,0 +1,9 @@
+package com.nexo;
+
+import javax.swing.*;
+
+public class Menu extends JFrame{
+    private JPanel pnl_Menu;
+    private JButton prisionerosButton;
+    private JButton guardiasButton;
+}

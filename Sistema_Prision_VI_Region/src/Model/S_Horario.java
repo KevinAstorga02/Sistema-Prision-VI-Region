b448126509c65aff91d00f7f68396d5e72b30df0
KevinAstorga02 <kevinastorgam02@gmail.com>
2020-11-30 23:47:29 -0300

    Update Java Model

--- Sistema_Prision_VI_Region/src/Model/S_Horario.java
+++ Sistema_Prision_VI_Region/src/Model/S_Horario.java
@@ -1,6 +1,43 @@
 package Model;
 
+import java.sql.Time;
+
 public class S_Horario {
     private String tipo;
-    private
+    private Time inicio_horario;
+    private Time fin_horario;
+
+    public S_Horario(String tipo, Time inicio_horario, Time fin_horario) {
+        this.tipo = tipo;
+        this.inicio_horario = inicio_horario;
+        this.fin_horario = fin_horario;
+    }
+
+    public S_Horario() {
+
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Time getInicio_horario() {
+        return inicio_horario;
+    }
+
+    public void setInicio_horario(Time inicio_horario) {
+        this.inicio_horario = inicio_horario;
+    }
+
+    public Time getFin_horario() {
+        return fin_horario;
+    }
+
+    public void setFin_horario(Time fin_horario) {
+        this.fin_horario = fin_horario;
+    }
 }
--- Sistema_Prision_VI_Region/src/Model/Turnos.java
+++ Sistema_Prision_VI_Region/src/Model/Turnos.java
@@ -1,6 +1,43 @@
 package Model;
 
+import java.time.LocalDateTime;
+
 public class Turnos {
     private String tipo;
-    private
+    private LocalDateTime hora_inicio;
+    private LocalDateTime hora_fin;
+
+    public Turnos(String tipo, LocalDateTime hora_inicio, LocalDateTime hora_fin) {
+        this.tipo = tipo;
+        this.hora_inicio = hora_inicio;
+        this.hora_fin = hora_fin;
+    }
+
+    public Turnos() {
+
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDateTime getHora_inicio() {
+        return hora_inicio;
+    }
+
+    public void setHora_inicio(LocalDateTime hora_inicio) {
+        this.hora_inicio = hora_inicio;
+    }
+
+    public LocalDateTime getHora_fin() {
+        return hora_fin;
+    }
+
+    public void setHora_fin(LocalDateTime hora_fin) {
+        this.hora_fin = hora_fin;
+    }
 }
